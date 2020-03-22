d098d755d3dc11271c461e17c8454e607fc2a397
Justyna Pietryga <justyna.pietryga@gmail.com>
2020-03-22 21:59:12 +0100

    02-JSCoreConcepts - heron.js task

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -1,11 +1,13 @@
 // boki trójkąta
-//a = 3;
-b = 4;
-c = 5;
-h = 7
+let a = 4;
+let b = 5;
+let c = 7;
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
 
-let a = () => "aaaa";
+let calculateTriangleArea = (a, b, c) => {
+    let p = (a + b + c) / 2;
+    return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+};
 
-console.log( a() );
+console.log(`Pole trójkąta o bokach: ${a}, ${b}, ${c}, wynosi ${calculateTriangleArea(a, b, c)}`);
