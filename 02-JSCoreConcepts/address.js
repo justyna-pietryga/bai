cdb63aa31682efcfa38edf6e354c6120937e9aaa
Justyna Pietryga <justyna.pietryga@gmail.com>
2020-03-22 21:46:10 +0100

    02-JSCoreConcepts - address.js task

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -6,7 +6,10 @@ city = 'Kraków';
 zip = '30-781';
 country = 'Poland';
 
-
+console.log("First way:\n");
+console.log(title + " " + name + " " + surname + "\nul." + street + "\n" + zip + " " + city + "\n" + country.toUpperCase());
+console.log("\nSecond way:\n");
+console.log(`${title} ${name} ${surname}\nul. ${street}\n${zip} ${city}\n${country.toUpperCase()}`);
 /* 
 dr Mateusz Kubicki
 ul. Długa 15
